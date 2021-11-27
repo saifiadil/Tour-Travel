2878ef52f5c54a411e452992c58b80559fb38c6a
saifiadil <adilsaifi966@gmail.com>
2021-11-27 00:02:25 -0800

    Tour & Travel

---
+++ first.js
@@ -0,0 +1,37 @@
+// const togglebutton =document.getElementsByClassName('toggle-button')[0]
+//         const navbarlink =document.getElementsByClassName('navbar-link')[0]
+//         togglebutton.addEventListener('click',()=>{
+//             navbarlink.classList.toggle('active')
+//         })
+
+        
+
+const a= (name) =>{
+    console.log("nama is :", name)
+
+}
+a("Adil");
+
+
+const add=(a,b)=>{
+    console.log("add two number",a+b)
+}
+add(5,6);
+
+const var1 = function(){
+    var a =12
+
+var add10 =a+10
+return add10;
+
+
+}
+var  name  = "samsung s10"
+console.log(name[6]);
+console.log(name.charAt(1))
+
+var firstname= "Adil";
+var secondname="saifi";
+var name = firstname + " " + secondname;
+console.log("name is :", name)
+
